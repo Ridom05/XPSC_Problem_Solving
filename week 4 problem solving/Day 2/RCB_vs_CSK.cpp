aa80b5b7b4f50ea29e5fa4ed65aa53ae2dbb766a
Ridom05 <montasirnawaz@gmail.com>
2024-11-17 15:54:34 +0600

    Add files via upload
    Week 4 problem solving

---
+++ week 4 problem solving/Day 2/RCB_vs_CSK.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int X, Y;
+    cin >> X >> Y;
+    int ans = abs(X - Y);
+    if (ans >= 18)
+    {
+        cout << "RCB" << endl;
+    }
+    else
+    {
+        cout << "CSK" << endl;
+    }
+    return 0;
+}
